493c8917421cbd574de42fe5c420013b471b9d5c
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-03-22 10:51:46 +0300

    Two-pass label-components

--- src/utils.lisp
+++ src/utils.lisp
@@ -32,14 +32,49 @@ the range of @c(bit) type."))
   (float (/ (count 0 (aops:flatten array))
             (array-total-size array))))
 
+(deftype sets-map () '(simple-array alex:non-negative-fixnum (*)))
+
+(sera:-> make-sets-map (alex:non-negative-fixnum)
+         (values sets-map &optional))
+(defun make-sets-map (n)
+  (let ((map (make-array n :element-type 'alex:non-negative-fixnum)))
+    (loop for i below n do (setf (aref map i) i))
+    map))
+
+(sera:-> sets-find! (sets-map alex:non-negative-fixnum)
+         (values alex:non-negative-fixnum &optional))
+(defun sets-find! (map id)
+  (let ((parent (aref map id)))
+    (if (= parent id) id
+        (let ((root (sets-find! map parent)))
+          (setf (aref map id) root)))))
+
+(sera:-> sets-union! (sets-map alex:non-negative-fixnum alex:non-negative-fixnum)
+         (values alex:non-negative-fixnum &optional))
+(defun sets-union! (map id1 id2)
+  (let ((root1 (sets-find! map id1))
+        (root2 (sets-find! map id2)))
+    (setf (aref map root2) root1)))
+
 (sera:-> make-pattern (alex:positive-fixnum)
          (values list &optional))
 (defun make-pattern (dim)
-  (loop for c in '(-1 1) append
-        (loop for d1 below dim collect
-              (loop for d2 below dim collect
-                    (if (= d1 d2) c 0)))))
+  (loop for d1 below dim collect
+        (loop for d2 below dim collect
+              (if (= d1 d2) -1 0))))
 
+(sera:-> add-indices (list list)
+         (values list &optional))
+(declaim (inline add-indices))
+(defun add-indices (idx1 idx2)
+  (mapcar
+   (lambda (i j)
+     (declare (type fixnum i j))
+     (+ i j))
+   idx1 idx2))
+
+(sera:-> index (simple-array list)
+         (values fixnum &optional))
 (declaim (inline index))
 (defun index (array index)
   (let ((rank (array-rank array)))
@@ -52,6 +87,8 @@ the range of @c(bit) type."))
        (aref array (first index) (second index) (third index)))
       (t (apply #'aref array index)))))
 
+(sera:-> (setf index) (fixnum simple-array list)
+         (values fixnum &optional))
 (declaim (inline (setf index)))
 (defun (setf index) (val array index)
   (let ((rank (array-rank array)))
@@ -64,60 +101,88 @@ the range of @c(bit) type."))
        (setf (aref array (first index) (second index) (third index)) val))
       (t (setf (apply #'aref array index) val)))))
 
+(sera:-> neighbor ((simple-array fixnum) list)
+         (values fixnum &optional))
+(defun neighbor (array coords)
+  (if (some (lambda (x) (< x 0)) coords) -1
+      (index array coords)))
+
+(sera:-> non-background ((simple-array fixnum) list)
+         (values list fixnum &optional))
+(defun non-background (array neighbors)
+  (declare (optimize (speed 3)))
+  (labels ((%go (neighbors non-background min)
+             (declare (type fixnum min))
+             (if (null neighbors)
+                 (values non-background min)
+                 (let ((lbl (neighbor array (car neighbors))))
+                   (if (= lbl -1)
+                       (%go (cdr neighbors) non-background min)
+                       (%go (cdr neighbors)
+                            (cons (car neighbors) non-background)
+                            (min min lbl)))))))
+    (%go neighbors nil most-positive-fixnum)))
+
+(sera:-> label-components! ((simple-array fixnum))
+         (values (simple-array fixnum) &optional))
+(defun label-components! (image)
+  "Label connected components in a binary array @c(array). Zeros are
+labeled as @c(-1), ones are labeled with a positive label. This
+function modifies its argument."
+  (declare (optimize (speed 3)))
+  (let ((map (make-sets-map (reduce #'+ (aops:flatten image))))
+        (current-label 0)
+        (pattern (make-pattern (array-rank image))))
+    (declare (type fixnum current-label))
+    (si:do-iterator (idx (si:indices (array-dimensions image)))
+      (if (zerop (index image idx))
+          (setf (index image idx) -1)
+          (multiple-value-bind (non-background min-lbl)
+              (non-background
+               image (mapcar (lambda (offset) (add-indices idx offset)) pattern))
+            (cond
+              ((null non-background)
+               (setf (index image idx) current-label)
+               (incf current-label))
+              (t
+               (setf (index image idx) min-lbl)
+               (dolist (neighbor non-background)
+                 (sets-union! map min-lbl (index image neighbor))))))))
+
+    (loop for i below (array-total-size image)
+          for x = (row-major-aref image i)
+          when (/= x -1) do
+          (setf (row-major-aref image i)
+                (sets-find! map (row-major-aref image i)))))
+  image)
+
 (sera:-> label-components ((simple-array bit))
          (values (simple-array fixnum) &optional))
 (defun label-components (array)
   "Label connected components in a binary array @c(array). Zeros are
-left as is, ones are labeled with a positive label."
-  (declare (optimize (speed 3)))
-  (let* ((dimensions (array-dimensions array))
-         (lbls (make-array dimensions :element-type 'fixnum :initial-element -1))
-         (indices (si:indices dimensions))
-         (current-label 1)
-         (pattern (make-pattern (array-rank array))))
-    (declare (type fixnum current-label))
-    (si:do-iterator (idx indices)
-      (let (queue)
-        (flet ((push-in-queue (idx)
-                 (cond
-                   ;; If an element is a background element, label it as so
-                   ((zerop (the bit (index array idx)))
-                    (setf  (index lbls idx) 0)
-                    0)
-                   ;; If the element does not have a label, assign the current label
-                   ((= (the fixnum (index lbls idx)) -1)
-                    (setf (index lbls idx) current-label)
-                    (push idx queue)
-                    1)
-                   ;; Already has a label - skip
-                   (t 0))))
-          (loop with delta fixnum = (push-in-queue idx)
-                until (null queue) do
-                (let ((idx2 (pop queue)))
-                  (dolist (offset pattern)
-                    (let ((shifted
-                           (mapcar
-                            (lambda (i o d)
-                              (declare (type (signed-byte 32) i o d))
-                              (mod (+ i o) d))
-                            idx2 offset dimensions)))
-                      (push-in-queue shifted))))
-                finally (incf current-label delta)))))
-    lbls))
+labeled as @c(-1), ones are labeled with a positive label."
+  (label-components!
+   (aops:vectorize* 'fixnum
+       (array)
+     array)))
 
 (sera:-> histogram ((simple-array fixnum))
-         (values (simple-array alex:non-negative-fixnum (*)) &optional))
+         (values list &optional))
 (defun histogram (array)
   "Compute a histogram for an array of labels."
-  (let* ((max-label (reduce #'max (aops:flatten array)))
-         (histogram (make-array max-label
-                                :element-type 'alex:non-negative-fixnum
-                                :initial-element 0)))
-    (loop for i below (array-total-size array)
-          for x = (row-major-aref array i)
-          unless (zerop x) do
-          (incf (aref histogram (1- x))))
-    histogram))
+  (let ((table
+         (loop with histogram = (make-hash-table)
+               for i below (array-total-size array)
+               for x = (row-major-aref array i)
+               unless (= x -1) do
+               (incf (gethash x histogram 0))
+               finally (return histogram)))
+        hist)
+    (maphash
+     (lambda (k v)
+       (push (cons v k) hist))
+     table)
+    hist))
 
 (sera:-> only-one-cluster ((simple-array bit))
          (values (simple-array bit) &optional))
@@ -126,11 +191,11 @@ left as is, ones are labeled with a positive label."
 largest."
   (let* ((lbls (label-components (invert array)))
          (histogram (histogram lbls))
-         (max (reduce #'max histogram))
-         (pos (1+ (position max histogram :test #'=))))
+         (max (reduce #'max histogram :key #'car))
+         (label (cdr (assoc max histogram))))
     (aops:vectorize* 'bit
         (lbls)
-      (if (= lbls pos) 0 1))))
+      (if (= lbls label) 0 1))))
 
 ;; Ball packings and voronoi diagrams
 
