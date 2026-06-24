5ba11c5b1043152be82f4b1e5f6297594df04989
Vasily Postnicov <shamaz.mazum@gmail.com>
2026-06-24 10:50:10 +0300

    Add SYMMETRICP arg to NOISE-FILL

--- src/value-noise.lisp
+++ src/value-noise.lisp
@@ -16,13 +16,15 @@
 (sera:-> noise-fill (alex:positive-fixnum
                      (integer 1 3)
                      &key
-                     (:mul     single-float)
-                     (:thr     single-float)
-                     (:seed    fixnum))
+                     (:mul        single-float)
+                     (:thr        single-float)
+                     (:seed       fixnum)
+                     (:symmetricp boolean))
          (values (simple-array bit) &optional))
-(defun noise-fill (side dim &key (mul 1.0) (thr 0.5) (seed 123456))
+(defun noise-fill (side dim &key (mul 1.0) (thr 0.5) (seed 123456) symmetricp)
   "Fill a @c(dim)-dimensional array with a side @c(side) with a
-thresholded value noise."
+thresholded value noise. If @c(symmetricp) is @c(T) the resulted noise
+will have central symmetry."
   (declare (optimize (speed 3)))
   (let ((array (make-array (loop repeat dim collect side) :element-type 'bit))
         (indices (si:power (si:range 0 side) dim))
@@ -34,7 +36,8 @@ thresholded value noise."
                            (lambda (i)
                              (declare (type alex:non-negative-fixnum i))
                              (let* ((x  (/ i side))
-                                    (%x (if (< x 0.5) x (- (1- x)))))
+                                    (%x (if (or (not symmetricp) (< x 0.5))
+                                            x (- (1- x)))))
                                (* mul %x)))
                            idx))
                    thr)
